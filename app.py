2e93bee7dd5271026369234bdb175dd1d9fcd587
Akhilesh Dixit <akhileshdixit25@gmail.com>
2024-04-08 22:26:31 +0530

    more background changes

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ page_bg = """
         background-color: whitesmoke !important;
         margin-top: 0;
     }
-    .st-emotion-cache-1gulkj5{
+    [data-testid= "stFileUploaderDropzone"]{
         background-color: whitesmoke;
         border: 2px solid #0c330b;
         width: 110%;
